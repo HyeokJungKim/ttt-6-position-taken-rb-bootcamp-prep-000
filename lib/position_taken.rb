2d72988ad995453b803407ac02f48820a1c427b2
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-12-05 03:03:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index){
-  var boo
   if(board[index-1] == " " || board[index-1] == ""){
-    boo = false}}
-
-    return boo
+    false}
 end
